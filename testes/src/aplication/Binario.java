a365cab434e85a53e852900a3402d8682f58491f
Murilo Lapa <murilolapavasconcelos@gmail.com>
2026-04-02 11:40:32 -0300

    Update Binario.java

--- testes/src/aplication/Binario.java
+++ testes/src/aplication/Binario.java
@@ -17,6 +17,8 @@ public class Binario {
        double soma = 0;
         int N_Binario = 0;
         while(op != 3){
+            Binarios.clear();
+            soma=0;
             System.out.println("");
             System.out.println(menu);
             Scanner sc = new Scanner(System.in);
@@ -33,21 +35,19 @@ public class Binario {
                     System.out.print("O seu numero em Binario é: ");
                     //para rodar a lista de forma invertida ja que em binario voce tem que inverter a ordem dos valores
                     for(int i = tamanho - 1;i !=-1; i--) {
-                        System.out.print("\n"+Binarios.get(i)); }
-
+                        System.out.print(Binarios.get(i)); }break;
                 case 2:
                     System.out.println("qual numero binario você quer converter para decimal: ");
                     N_Binario = sc.nextInt();
                     String N_Btext = String.valueOf(N_Binario);//para tranformar em texto o numero
-
                     //para percorrer a lista e ver o que esta em cada caracthere
                     for(int i= 0; i < N_Btext.length()  ; i++){
                     if (N_Btext.charAt(i) == '1'){
                         int expoente = N_Btext.length() - 1 - i;
                          double calc =Math.pow(2, expoente);
                            soma = soma + calc; };
-
                     }
-                    System.out.println(soma);
+                    System.out.print("O seu numero em Decimal é: ");
+                    System.out.println(soma);break;
             }}}}
 
