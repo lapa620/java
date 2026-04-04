14aaf9e3cb08d5a42b6cc7be1495f340c8286ffa
Murilo Lapa <murilolapavasconcelos@gmail.com>
2026-04-04 15:47:45 -0300

    Enhance codificador with clear list and user prompts
    Added functionality to clear the list before encoding and improved user prompts.

--- testes/src/aplication/codificador.java
+++ testes/src/aplication/codificador.java
@@ -5,34 +5,45 @@ import java.util.Scanner;
 public class codificador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//lista que sera percorrida
+
 		ArrayList<Integer> Binarios = new ArrayList<>();
 		char[] letras = "xyzabcdefghijklmnopqrstuvw".toCharArray();
 		Scanner sc = new Scanner(System.in);
 		int op = 0;
-		
-		while(op !=3) {
+
+		while(op != 3) {
 			System.out.println("codificador");
 			System.out.println("[1]-codificar texto");
 			System.out.println("[2]-descodificar texto");
 			System.out.println("[3]-sair");
 			op = sc.nextInt();
-		switch (op) {
-		case 1:
-		System.out.println("escolha uma palavra para criptografar de forma simples");
-		String palavra =sc.next();
-		
-		for(int i = 0;i < palavra.length();i++) {
-			for(int j = 0;j < letras.length;j++) {
-				if (palavra.charAt(i) == letras[j]) {
-					Binarios.add((j+1));
-					int codificado = j+1;
-					
-				}
-			}}
-			
-		System.out.print(Binarios);}
-		System.out.println(" ");
+			switch (op) {
+			case 1:
+				Binarios.clear(); // limpa a lista
+				
+				System.out.println("escolha uma palavra para criptografar");
+				String palavra = sc.next();
+
+				for(int i = 0; i < palavra.length(); i++) {
+					for(int j = 0; j < letras.length; j++) {
+						if (palavra.charAt(i) == letras[j]) {
+							Binarios.add(j + 1);
+						}}}
+				System.out.println(Binarios);
+				break;
+			case 2:
+				System.out.println("Ainda não foi feito");
+				break;
+
+			case 3:
+				System.out.println("Saindo...");
+				break;
+
+			default:
+				System.out.println("Opção inválida");
+			}
+
+			System.out.println();
 		}
-	}}
+		
+}}
