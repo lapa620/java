9a666c38c5b0eb4a0364bef8fe8cdd8dd96a7058
Murilo Lapa <murilolapavasconcelos@gmail.com>
2026-04-06 10:35:55 -0300

    Update codificador.java

--- testes/src/aplication/codificador.java
+++ testes/src/aplication/codificador.java
@@ -4,46 +4,49 @@ import java.util.Scanner;
 
 public class codificador {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ArrayList<Integer> Binarios = new ArrayList<>();
-		char[] letras = "xyzabcdefghijklmnopqrstuvw".toCharArray();
-		Scanner sc = new Scanner(System.in);
-		int op = 0;
+        ArrayList<Integer> Binarios = new ArrayList<>();
+        char[] letras = "xyzabcdefghijklmnopqrstuvw".toCharArray();
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
 
-		while(op != 3) {
-			System.out.println("codificador");
-			System.out.println("[1]-codificar texto");
-			System.out.println("[2]-descodificar texto");
-			System.out.println("[3]-sair");
-			op = sc.nextInt();
-			switch (op) {
-			case 1:
-				Binarios.clear(); // limpa a lista
-				
-				System.out.println("escolha uma palavra para criptografar");
-				String palavra = sc.next();
+        while(op != 3) {
+            System.out.println("codificador");
+            System.out.println("[1]-codificar texto");
+            System.out.println("[2]-descodificar texto");
+            System.out.println("[3]-sair");
+            System.out.println("(Enter para poder seleconar as opçoes: )");
+            sc.nextLine();//para limpar o scanner
+            System.out.println("escolha uma das opçoes");
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    Binarios.clear(); // limpa a lista
 
-				for(int i = 0; i < palavra.length(); i++) {
-					for(int j = 0; j < letras.length; j++) {
-						if (palavra.charAt(i) == letras[j]) {
-							Binarios.add(j + 1);
-						}}}
-				System.out.println(Binarios);
-				break;
-			case 2:
-				System.out.println("Ainda não foi feito");
-				break;
+                    System.out.println("escolha uma palavra para criptografar");
+                    String palavra = sc.next();
 
-			case 3:
-				System.out.println("Saindo...");
-				break;
+                    for(int i = 0; i < palavra.length(); i++) {
+                        for(int j = 0; j < letras.length; j++) {
+                            if (palavra.charAt(i) == letras[j]) {
+                                Binarios.add(j + 1);
+                            }}}
+                    System.out.println(Binarios);
+                    break;
+                case 2:
+                    System.out.println("Ainda não foi feito");
+                    break;
 
-			default:
-				System.out.println("Opção inválida");
-			}
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
 
-			System.out.println();
-		}
-		
-}}
+                default:
+                    System.out.println("Opção inválida");
+            }
+
+            System.out.println();
+        }
+
+    }}
