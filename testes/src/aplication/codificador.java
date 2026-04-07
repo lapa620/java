afd13b7ddc4c5b430494a46f78820954822e0718
Murilo Lapa <murilolapavasconcelos@gmail.com>
2026-04-07 09:05:22 -0300

    Update codificador.java

--- testes/src/aplication/codificador.java
+++ testes/src/aplication/codificador.java
@@ -4,49 +4,59 @@ import java.util.Scanner;
 
 public class codificador {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> codigo = new ArrayList<>();
-        char[] letras = "xyzabcdefghijklmnopqrstuvw".toCharArray();
-        Scanner sc = new Scanner(System.in);
-        int op = 0;
+        public static void main(String[] args) {
+            ArrayList<Integer> codigo = new ArrayList<>();
+            char[] letras = "xyzabcdefghijklmnopqrstuvw".toCharArray();
+            Scanner sc = new Scanner(System.in);
+            int op = 0;
+            int ops;
+            while(op != 3) {
+                System.out.println("codificador");
+                System.out.println("[1]-codificar texto");
+                System.out.println("[2]-descodificar texto");
+                System.out.println("[3]-sair");
+                System.out.println("(Enter para poder seleconar as opçoes: )");
+                sc.nextLine();//para limpar o scanner
+                System.out.println("escolha uma das opçoes");
+                op = sc.nextInt();
+                switch (op) {
+                    case 1:
+                        codigo.clear(); // limpa a lista
+                        System.out.println("escolha uma palavra para criptografar");
+                        String palavra = sc.next();
 
-        while(op != 3) {
-            System.out.println("codificador");
-            System.out.println("[1]-codificar texto");
-            System.out.println("[2]-descodificar texto");
-            System.out.println("[3]-sair");
-            System.out.println("(Enter para poder seleconar as opçoes: )");
-            sc.nextLine();//para limpar o scanner
-            System.out.println("escolha uma das opçoes");
-            op = sc.nextInt();
-            switch (op) {
-                case 1:
-                    codigo.clear(); // limpa a lista
-                    System.out.println("escolha uma palavra para criptografar");
-                    String palavra = sc.next();
+                        for(int i = 0; i < palavra.length(); i++) {//para percorrer cada letra da palavra
+                            //para comparar as letras com o alfabeto
+                            for(int j = 0; j < letras.length; j++) {
+                                if (palavra.charAt(i) == letras[j]) {
+                                    codigo.add(j);
+                                }}}
+                        System.out.println(codigo);
+                        break;
+                    case 2:
+                        System.out.println("deseja escrever uma mensgem ou decodificar a mensagem feita aqui");
+                        System.out.println("[1]-mensagem anterior   [2]-nova mensagem");
+                        ops = sc.nextInt();
+                        switch (ops){
+                            case 1:
+                        for(int i = 0; i < codigo.size(); i++) {
+                            int num = codigo.get(i);
+                            System.out.print(letras[num]);
+                        }
+                        System.out.println();
+                        break;
+                            case 2:
+                            System.out.print("em processo...")
+                                break;
 
-                    for(int i = 0; i < palavra.length(); i++) {//para percorrer cada letra da palavra
-                         //para comparar as letras com o alfabeto
-                        for(int j = 0; j < letras.length; j++) {
-                            if (palavra.charAt(i) == letras[j]) {
-                                codigo.add(j);
-                            }}}
-                    System.out.println(codigo);
-                    break;
-                case 2:
-                    for(int i = 0; i < codigo.size(); i++) {
-                        int num = codigo.get(i);
-                        System.out.print(letras[num]);
-                    }
-                    System.out.println();
-                    break;
-                case 3:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida");
+                        }break;
+                    case 3:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
 
-    }}
+        }}
